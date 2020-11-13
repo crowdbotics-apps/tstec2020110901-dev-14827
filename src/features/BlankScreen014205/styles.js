4bfe08309fcb2a273927a8d5a23cf8d3ca3d55a8
evan-carlsen <evan@crowdbotics.com>
2020-11-13 01:54:33 -0800

    Update screen

--- src/features/BlankScreen014205/styles.js
+++ src/features/BlankScreen014205/styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native'
 
-import {scaleVertical, scale} from "../../../utils/scale";
+import {scaleVertical, scale} from "../../utils/scale";
 
 export const styles = StyleSheet.create({
     container: {
