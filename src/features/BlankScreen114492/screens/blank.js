0773954ce54f296c01273e0fe42dc2f1c5d3ff8e
evan-carlsen <evan@crowdbotics.com>
2020-11-13 01:06:37 -0800

    Update screen

--- src/features/BlankScreen114492/screens/blank.js
+++ src/features/BlankScreen114492/screens/blank.js
@@ -15,6 +15,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Slider from '@react-native-community/slider';
 import { CheckBox } from 'react-native-elements';
 import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+import logo from '../assets/logo.png';
 
 export default class Blank extends React.Component {
   static navigationOptions = ({navigation}) => {
@@ -27,7 +28,16 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>This is your new component</Text>
+                  <View style={styles.itemsContainer}>
+                <Image
+                    style={{
+                    width: 365,
+                    height: 161,
+                }}
+                    resizeMode="contain"
+                    source={logo}/>
+                <Text style={styles.heading}>{"Splash Screen Text"}</Text>
+            </View>
     </View>
   );
 }
