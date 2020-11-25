e06026f673d6086166a84f9405c8420765869b2b
Crowdbotics <team@crowdbotics.com>
2020-11-25 05:39:18 +0000

    add component EmailAuth

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,12 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-{ name: 'BlankScreen1214829', human_name: 'Blank Screen 12', access_route: 'BlankScreen1214829'},
-{ name: 'BlankScreen1014646', human_name: 'Blank Screen 10', access_route: 'BlankScreen1014646'},
-{ name: 'BlankScreen614530', human_name: 'Blank Screen 6', access_route: 'BlankScreen614530'},
-{ name: 'BlankScreen414519', human_name: 'Blank Screen 4', access_route: 'BlankScreen414519'},
-{ name: 'BlankScreen214517', human_name: 'Blank Screen 2', access_route: 'BlankScreen214517'},
-{ name: 'BlankScreen114492', human_name: 'Blank Screen 1', access_route: 'BlankScreen114492'},
-{ name: 'BlankScreen014205', human_name: 'Blank Screen 0', access_route: 'BlankScreen014205'},
+{ name: 'EmailAuth', human_name: 'Sign In / Sign Up', access_route: 'EmailAuth'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
