885dad83295fe6caa824ad070f62e767b8aeddcd
Crowdbotics <team@crowdbotics.com>
2020-11-25 05:56:13 +0000

    add component Splash Screen - TEST SHARE

---
+++ src/features/SplashScreen-TestShare1514944/navigator.js
@@ -0,0 +1,13 @@
+import { createStackNavigator } from "react-navigation-stack";
+
+import Blank from "./screens/blank";
+
+export default BlankBlueprintNavigator = createStackNavigator(
+  {
+    Blank: { screen: Blank }
+  },
+  {
+    initialRouteName: "Blank",
+    defaultNavigationOptions: ({ navigation }) => ({ header: null }),
+  }
+);
